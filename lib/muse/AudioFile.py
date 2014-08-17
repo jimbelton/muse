eac3f4770645d7bf8abcf6ef2b03d2091a3f60fd
jimbelton <jim.belton@gmail.com>
2014-08-17 00:25:32 -0700

    Allow reconcile to run on all files in library

--- lib/muse/AudioFile.py
+++ lib/muse/AudioFile.py
@@ -8,56 +8,56 @@ from muse.MuseFile import MuseFile
 class AudioFile(MuseFile):
     dirPattern  = re.compile(r'(?:\./)?(?:([^/])/)?(?:([^/]+)/)?(?:.+/)?([^/]*)?$')
     filePattern = re.compile(r'(?:([^-]+?)\s*-\s+)?(?:([^-]+?)\s*-\s+)?(?:([^-]+?)\s*-\s+)?(?:.*-\s+)?(.+)\..+$')
-    
+
     def __init__(self, filePath):
         MuseFile.__init__(self, filePath)
         self.md5 = None
-        
+
         # Determine meta-data from file path
-        
+
         (dirName, fileName) = os.path.split(filePath)
         match = AudioFile.dirPattern.match(dirName)
-        
+
         if not match:
             sys.exit("AudioFile: Failed to parse directory name " + dirName)
 
         self.dirLetter = match.group(1)
         self.dirArtist = match.group(2)
         self.dirAlbum  = match.group(3)
-        
+
         if not match.group(2) and match.group(3):
             self.dirAlbum  = None
-            
+
             if self.dirLetter or len(match.group(3)) > 1:
                 self.dirArtist = match.group(3)
             else:
                 self.dirLetter = match.group(3)
-       
+
         #print "Directory: Letter %s Artist %s Album %s" % (self.dirLetter, self.dirArtist, self.dirAlbum)
         match = AudioFile.filePattern.match(fileName)
-        
+
         if not match:
             sys.exit("AudioFile: Failed to parse file name " + fileName)
-    
+
         self.fileArtist = match.group(1)
         self.fileAlbum  = match.group(2)
         self.fileTrack  = match.group(3)
         self.fileTitle  = match.group(4)
-        
+
         if not match:
             sys.exit("AudioFile: Failed to parse file name " + dirName)
-            
+
         self.score = 0
-        
+
         if self.dirArtist:
             self.score += 1 if self.dirLetter and self.dirArtist.startswith(self.dirLetter) else 0
             self.score += 1 if self.dirArtist == self.fileArtist else 0
-        
+
         if self.fileArtist:
             self.score += 1 if self.dirLetter and self.fileArtist.startswith(self.dirLetter) else 0
-        
+
         #print "File name: Letter %s Artist %s Album %s Track %s Song %s" % (self.dirLetter, self.fileArtist, self.fileAlbum, self.fileTrack, self.fileTitle)
-        
+
     # Override this base method in derived classes. This method includes the entire file in the audioMd5 checksum
     #
     def readFile(self):
@@ -68,7 +68,7 @@ class AudioFile(MuseFile):
         self.md5      = md5.new(self.stream.read())
         self.audioMd5 = self.md5
         self.close()
-        
+
     def compare(self, other):
         self.readFile()
         other.readFile()
@@ -81,20 +81,22 @@ class AudioFile(MuseFile):
 
     def isPreferredTo(self, other):
         #print "%s %s %s %s %s" % (self.dirLetter, self.dirArtist, self.dirAlbum, self.fileArtist, self.fileTitle)
-        
+
         if self.getSize() == other.getSize():
             if self.compareAudio(other):
                 if self.compare(other):
                     print "Identical files " + self.filePath + " and " + other.filePath
                 else:
                     print "Identical audio in files " + self.filePath + " and " + other.filePath
-                    
+
             else:
                 print "Same sized files " + self.filePath + " and " + other.filePath
-            
-            return other.score - self.score 
-             
+
+            return other.score - self.score
+
         else:
             print "Same named files " + self.filePath + " and " + other.filePath
             return other.score - self.score if getOption('forced', default = False) else 0
 
+    def reconcile(self):
+        sys.stderr.write("%s: warning: %s does not support reconcile\n" % (__file__, self.filePath))
--- lib/muse/Mp3File.py
+++ lib/muse/Mp3File.py
@@ -8,10 +8,10 @@ from muse.Options   import getOption
 
 def bigEndianInteger(byteString):
     integer = 0
-    
+
     for byte in byteString:
         integer = integer * 8 + ord(byte)
-    
+
     return integer
 
 class Mp3FileError(Exception):
@@ -24,95 +24,101 @@ class Mp3FileError(Exception):
         return  "%s(%d): %s" % (self.filePath, self.offset, self.message)
 
 # ID3v2 string encoding types are the byte values 0 ... 3
-# 
+#
 id3v2EncodingToPythonEncoding = ("iso-8859-1", "utf_16", "utf_16_be", "utf_8")
 
 class Mp3File(AudioFile):
-    
+
     def expect(self, description, actual, expected):
         if getOption('warning') and actual != expected:
-            sys.stderr.write("warning: %s(%d): %s was %s but expected %s\n" 
+            sys.stderr.write("warning: %s(%d): %s was %s but expected %s\n"
                              % (self.filePath, self.stream.tell(), description, actual, expected))
-        
+
         return actual
-    
+
     def expectMember(self, description, actual, expectedSet):
         if getOption('warning') and not actual in expectedSet:
             sys.stderr.write("warning: %s(%d): %s was %s but required to be in %s\n"
+
                              % (self.filePath, self.stream.tell(), description, actual, expectedSet))
-                               
-        return actual                      
+
+        return actual
 
     def expectPadding(self, padding):
         for paddingChar in padding:
-            self.expect("padding byte", ord(paddingChar), 0)
-            
+            if self.expect("padding byte", ord(paddingChar), 0) != 0:
+                if padding.find("\xFF\xFF\xFF") >= 0:
+                    raise Mp3FileError(self.filePath, self.stream.tell() - len(padding) + padding.find("\xFF\xFF\xFF"),
+                                       "Found an MP3 sync tag in ID3 tag padding")
+
+                break
+
         return padding
-        
+
     def require(self, description, actual, expected):
         if actual != expected:
             raise Mp3FileError(self.filePath, self.stream.tell(), "%s was %s but required %s" % (description, actual, expected))
-        
+
         return actual
-        
+
     def requireMember(self, description, actual, expectedSet):
         if not actual in expectedSet:
-            raise Mp3FileError(self.filePath, self.stream.tell(), 
+            raise Mp3FileError(self.filePath, self.stream.tell(),
                                "%s was %s but required to be in %s"  % (description, actual, expectedSet))
-        
+
         return actual
 
     def readFromTag(self, length, message):
         if length > self.tagLeft:
-            raise Mp3FileError(self.filePath, self.stream.tell(), 
+            raise Mp3FileError(self.filePath, self.stream.tell(),
                                "%s: length to read %d exceeds remaining tag length %d" % (message, length, self.tagLeft))
-                               
+
         self.tagLeft -= length
         data = self.read(length, message)
         self.md5.update(data)
         return data
-      
+
     # Return: true if read successfully, false if not
     def readID3v2Frame(self):
         frameHeader = self.readFromTag(10, "ID3v2 frame header")
-        
+
         if ord(frameHeader[:1]) == 0:
             self.expectPadding(frameHeader[1:])
             return
-            
+
         frameId         = frameHeader[:4]
         frameBodyLength = bigEndianInteger(frameHeader[4:8])
         frameFlags      = bigEndianInteger(frameHeader[9:10])
-        
+
         if frameFlags & 0x20:
             self.readFromTag(1, "ID3v2 frame group identifier byte")
-            
+
         try:
             frameBody = self.readFromTag(frameBodyLength, "ID3v2 '%s' frame body" % (frameId))
-        
+
         except Mp3FileError as error:
             if getOption('warning'):
                 sys.stderr.write("warning: " + str(error) + ": skipping remainder of ID3v2 tag\n")
-                
+
             return False
-        
-        if (self.expect("ignoring '%s' frame: compression flag" % (frameId), frameFlags & 0x80, 0) or 
+
+        if (self.expect("ignoring '%s' frame: compression flag" % (frameId), frameFlags & 0x80, 0) or
             self.expect("ignoring '%s' frame: encryption flag"  % (frameId), frameFlags & 0x40, 0)):
             return True
-            
+
         if frameId[0] == "T" or frameId == "IPLS":
             encoding = ord(frameBody[0])
             self.requireMember("encoding descriptor for %s frame" % (frameId), encoding, {0, 1, 2, 3})
             value = frameBody[1:].decode(id3v2EncodingToPythonEncoding[encoding], errors="strict")
-                
+
         else:
             value = frameBody
-            
+
         self.frames[frameId] = value
         return True
 
     def readID3v2Tag(self, header):
-        header += self.read(6, "ID3v2 header")
+        header += self.read(7, "ID3v2 header")
         self.md5.update(header)
         self.id3v2version = self.expectMember("version", "ID3v2.%d.%d" % (ord(header[3]), ord(header[4])),
                                               {"ID3v2.3.0", "ID3v2.4.0"})
@@ -122,64 +128,73 @@ class Mp3File(AudioFile):
         self.expect("experimental flag", tagFlags & 0x20, 0)
         self.require("undefined tag flags", tagFlags & 0x1F, 0)
         self.id3Length = 0
-        
+
         for char in header[6:]:
             self.id3Length <<= 7
             self.id3Length +=  ord(char)
-            
+
         self.tagLeft    = self.id3Length
         self.id3Length += 10
-            
+
         # If the extended header flag is set
         if tagFlags & 0x40:
             extendedHeader       = self.readFromTag(10, "ID3v2 tag extended header")
-            extendedHeaderLength = self.expectedSet("extended head length", bigEndianInteger(extendedHeader[:4]), {6, 10})
-            
+            extendedHeaderLength = self.expectMember("extended head length", bigEndianInteger(extendedHeader[:4]), {6, 10})
+
             if extendedHeaderLength == 10:
                 crc = self.readFromTag(4, "ID3v2 tag CRC")
-                
-        self.frames = {}
 
         while self.tagLeft >= 10 and self.readID3v2Frame():
             pass
 
-        body = self.readFromTag(self.tagLeft, "ID3v2 padding")
-        self.md5.update(body)
-        
+        padding = self.readFromTag(self.tagLeft, "ID3v2 padding")
+        self.expectPadding(padding)
+
     def readFile(self):
         if self.md5:
             return
 
         self.open()
-        self.md5 = md5.new()
-        
+        self.md5    = md5.new()
+        self.frames = {}
+
         while True:
-            header = self.read(4, "header tag", {'eof'})
-            
+            header = self.read(3, "header tag", {'eof'})
+
             if len(header) == 0:    # EOF
-                raise Mp3FileError(self.filePath, self.stream.tell(), "EOF without any mp3 content")
-            
-            if (header[:3] == "ID3"):
+                raise Mp3FileError(self.filePath, self.stream.tell(), "EOF without any MP3 content")
+
+            if (header == "ID3"):
                 self.readID3v2Tag(header)
                 continue
-                
-            words = struct.unpack(">l", header)
-            
-            if (words[0] & 0xFFFF0000) != 0xFFFB0000:
-                raise Mp3FileError(self.filePath, self.stream.tell(),
-                                   "Unknown tag '%04x%04x' (ID3v2 tag length %d)"
-                                   % ((words[0] >> 4) & 0xFFFF, words[0] & 0xFFFF, self.id3Length))
+
+            # Check for sync bytes
+            #
+            if header[:2] != "\xFF\xFB":
+                discarded = 1
+                header = header[1:]
+
+                while header != "\xFF\xFB":
+                    discarded += 1
+                    header = header[1] + self.read(1, "sync byte", {'eof'})
+
+                    if len(header) < 2:
+                        raise Mp3FileError(self.filePath, self.stream.tell(), "EOF searching for MP3 sync bytes")
+
+                if getOption('warning'):
+                    sys.stderr.write("warning: %s(%d): Found sync bytes after skipping %d bytes\n" %
+                                     (self.filePath, self.stream.tell(), discarded))
+
             self.md5.update(header)
-                
             self.audioMd5 = md5.new(header)
             remainder     = self.stream.read()
             self.md5.update(remainder)
             self.audioMd5.update(remainder)
             break
-    
-        #print "'" + self.frames['TPE1'] + "' == '" + (self.dirArtist if self.dirArtist else "") + "'" 
+
+        #print "'" + self.frames['TPE1'] + "' == '" + (self.dirArtist if self.dirArtist else "") + "'"
         self.score += 1 if self.frames.get('TPE1') == self.dirArtist  else 0
-        self.score += 1 if self.frames.get('TPE1') == self.fileArtist else 0        
+        self.score += 1 if self.frames.get('TPE1') == self.fileArtist else 0
         self.score += 1 if self.frames.get('TALB') == self.dirAlbum   else 0
         self.score += 1 if self.frames.get('TALB') == self.fileAlbum  else 0
         self.score += 1 if self.frames.get('TRCK') == self.fileTrack  else 0
@@ -188,8 +203,7 @@ class Mp3File(AudioFile):
 
     def reconcile(self):
         self.readFile()
-        
+
         if self.dirArtist == self.fileArtist:
-            if self.frames['TPE1'] != self.fileArtist:
-                print "%s: artist tag %s differs from directory artist %s" % (self.filePath, self.frames['TPE1'], self.fileArtist)
-                    
+            if self.frames.get('TPE1') != self.fileArtist:
+                print "%s: artist tag %s differs from directory artist %s" % (self.filePath, self.frames.get('TPE1'), self.fileArtist)
